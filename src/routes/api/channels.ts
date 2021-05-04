359b751b0c9be18f2640208d1272b5eec3482950
wand2016 <d19921207@gmail.com>
2021-05-04 17:10:59 +0900

    配信中ユーザ取得

---
+++ src/routes/api/channels.ts
@@ -0,0 +1,18 @@
+import express, { Request } from "express";
+import { findUserIdsByUserNames } from "@/users";
+import { findChannelsByUserLoginNames } from "@/channels";
+
+const router = express.Router();
+
+type Params = {
+  names: string[];
+};
+
+router.get("/search", async (req: Request<any, any, any, Params>, res) => {
+  const channels = await findChannelsByUserLoginNames(req.query.names ?? []);
+  res.send({
+    data: channels,
+  });
+});
+
+export default router;
--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import users from "./users";
+import channels from "./channels";
 
 const router = express.Router();
 
 router.use("/users", users);
+router.use("/channels", channels);
 
 export default router;

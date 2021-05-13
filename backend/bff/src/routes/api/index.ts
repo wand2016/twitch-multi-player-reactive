4ff290ad1ae409a2f27d8db70cdbd923573e9c38
wand2016 <d19921207@gmail.com>
2021-05-14 01:36:43 +0900

    bffのroute定義修正 -- path parameter取れてなかった

--- backend/bff/src/routes/api/index.ts
+++ backend/bff/src/routes/api/index.ts
@@ -1,10 +1,8 @@
 import express from "express";
 import streamers from "./streamers";
-import monitoring from "./monitoring";
 
 const router = express.Router();
 
 router.use("/streamers", streamers);
-router.use("/streamers/:streamerId/monitoring", monitoring);
 
 export default router;
--- backend/bff/src/routes/api/streamers.ts
+++ backend/bff/src/routes/api/streamers.ts
@@ -1,6 +1,7 @@
 import express, { Request } from "express";
 import { components, paths } from "@lib/types/schema-mine";
 import { findStreamersByLoginNames } from "@bff/services/streamer";
+import { monitorStreamer } from "@bff/services/monitoring";
 
 const router = express.Router();
 
@@ -21,6 +22,19 @@ router.get(
       data: streamers,
     };
     res.send(responseBody);
+  }
+);
+
+router.put(
+  "/:streamerId/monitoring",
+  async (
+    req: Request<
+      paths["/streamers/{streamerId}/monitoring"]["put"]["parameters"]["path"]
+    >,
+    res
+  ) => {
+    await monitorStreamer(req.params.streamerId);
+    res.send("sub ok");
   }
 );
 

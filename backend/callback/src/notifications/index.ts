68d4938f891dadaa8f5070e30bbb890e7d10e489
wand2016 <d19921207@gmail.com>
2021-05-15 16:24:40 +0900

    分割するほどのことのないモジュール統廃合

--- backend/callback/src/notifications/index.ts
+++ backend/callback/src/notifications/index.ts
@@ -1,4 +1,4 @@
-import { getWSClient } from "@callback/websocket";
+import Pusher from "pusher";
 
 export type Notification = {
   type: "stream.online" | "stream.offline";
@@ -9,7 +9,29 @@ export type Notify = {
   (notification: Notification): Promise<void>;
 };
 
-// TODO: 自前でWebSocketサーバ用意して遊ぶかも
+// TODO: pusherやめて自前でWebSocketサーバ用意して遊ぶかも
+function createWSClient() {
+  return new Pusher({
+    appId: process.env.PUSHER_APP_ID ?? "",
+    key: process.env.PUSHER_KEY ?? "",
+    secret: process.env.PUSHER_SECRET ?? "",
+    cluster: process.env.PUSHER_CLUSTER ?? "",
+    useTLS: true,
+  });
+}
+
+export const getWSClient = (() => {
+  let pusher: Pusher;
+
+  return () => {
+    if (!pusher) {
+      pusher = createWSClient();
+    }
+
+    return pusher;
+  };
+})();
+
 export const notify: Notify = async (notification: Notification) => {
   await getWSClient().trigger(
     process.env.PUSHER_CHANNEL ?? "",

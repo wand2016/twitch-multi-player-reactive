f96ccec8fa34d994268c0ef67b60f0647df860c0
wand2016 <d19921207@gmail.com>
2021-05-15 23:41:33 +0900

    リクエストヘッダ小文字対応

--- backend/src/callback/signature/__tests__/index.spec.ts
+++ backend/src/callback/signature/__tests__/index.spec.ts
@@ -19,12 +19,37 @@ const sampleHeader = {
   "X-Forwarded-Proto": "https",
 };
 
+const sampleHeaderLower = {
+  host: "de13bc876b02.ngrok.io",
+  "user-agent": "Go-http-client/1.1",
+  "content-length": "394",
+  "accept-encoding": "gzip",
+  "content-type": "application/json",
+  "twitch-eventsub-message-id": "cb4a5bcc-b820-4c7f-a5f5-c6eb4c3cf3b8",
+  "twitch-eventsub-message-retry": "0",
+  "twitch-eventsub-message-signature":
+    "sha256=894a51ef043f00316786ddff17a2c8b1d69b190ab297838895bd68d89be2955b",
+  "twitch-eventsub-message-timestamp": "2021-05-15T08:18:54.603385505Z",
+  "twitch-eventsub-message-type": "webhook_callback_verification",
+  "twitch-eventsub-subscription-is-batching-enabled": "false",
+  "twitch-eventsub-subscription-type": "stream.online",
+  "twitch-eventsub-subscription-version": "1",
+  "x-forwarded-for": "54.184.54.118",
+  "x-forwarded-proto": "https",
+};
+
 const sampleBody =
   '{"subscription":{"id":"9ae4c23e-a5ea-4143-9492-d27a4ce85066","status":"webhook_callback_verification_pending","type":"stream.online","version":"1","condition":{"broadcaster_user_id":"634932612"},"transport":{"method":"webhook","callback":"https://de13bc876b02.ngrok.io/callback"},"created_at":"2021-05-15T08:18:54.598197448Z","cost":1},"challenge":"piy_wknaGMbyRFyl4jCM1grNvqdbEUf6q-Fr6n484wY"}';
 
 test("signature match", () => {
   expect(() => {
     verifySignature(sampleHeader, sampleBody, "123456789a");
+  }).not.toThrow(SignatureMismatch);
+});
+
+test("signature match for lower", () => {
+  expect(() => {
+    verifySignature(sampleHeaderLower, sampleBody, "123456789a");
   }).not.toThrow(SignatureMismatch);
 });
 
--- backend/src/callback/signature/index.ts
+++ backend/src/callback/signature/index.ts
@@ -1,9 +1,15 @@
 import crypto from "crypto";
 
+type Headers = Record<string, string | undefined>;
+
 export class SignatureMismatch extends Error {
   constructor(expected: string, actual: string) {
     super(`signature mismatch. expected: ${expected}, actual: ${actual}`);
   }
+}
+
+function getHeader(headers: Headers, key: string): string {
+  return headers[key] ?? headers[key.toLowerCase()] ?? "";
 }
 
 /**
@@ -11,19 +17,19 @@ export class SignatureMismatch extends Error {
  * @throws SignatureMismatch 署名検証に失敗したら送出
  */
 export function verifySignature(
-  requestHeader: Record<string, string | undefined>,
+  requestHeaders: Record<string, string | undefined>,
   requestBody: string,
   secret: string
 ): void {
   const hmacMessage =
-    (requestHeader["Twitch-Eventsub-Message-Id"] ?? "") +
-    (requestHeader["Twitch-Eventsub-Message-Timestamp"] ?? "") +
+    getHeader(requestHeaders, "Twitch-Eventsub-Message-Id") +
+    getHeader(requestHeaders, "Twitch-Eventsub-Message-Timestamp") +
     requestBody;
 
   const signature = crypto.createHmac("sha256", secret).update(hmacMessage);
 
   const expected = `sha256=${signature.digest("hex")}`;
-  const actual = requestHeader["Twitch-Eventsub-Message-Signature"] ?? "";
+  const actual = getHeader(requestHeaders, "Twitch-Eventsub-Message-Signature");
 
   if (expected !== actual) {
     throw new SignatureMismatch(expected, actual);
